c1f7fc812e71f1512b82e7f3480531fa930df550
Sebastian Mora <122997483+sebastianmora29@users.noreply.github.com>
2025-05-24 15:54:26 -0300

    actualiza db.php
    Implementa la conexión al servidor de base de datos MySQL

--- db.php
+++ db.php
@@ -1,18 +1,12 @@
 <?php
-header('Content-Type: application/json');
+$host = "localhost";
+$user = "root";
+$password = "";
+$dbname = "portfolio";
 
-$response = ['success' => false, 'mensaje' => ''];
+$conn = new mysqli($host, $user, $password, $dbname);
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $nombre = filter_input(INPUT_POST, 'nombre', FILTER_SANITIZE_STRING);
-  $email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
-  $mensaje = filter_input(INPUT_POST, 'mensaje', FILTER_SANITIZE_STRING);
-  
-  // Aquí normalmente guardarías en base de datos
-  // Simulamos una respuesta exitosa
-  $response['success'] = true;
-  $response['mensaje'] = "Gracias $nombre, hemos recibido tu mensaje.";
+if ($conn->connect_error) {
+    die("Conexión fallida: " . $conn->connect_error);
 }
-
-echo json_encode($response);
 ?>
