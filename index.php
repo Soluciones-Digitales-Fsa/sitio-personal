9d1f592bae772af574db9354aec9df01291d6bed
Sebastian Mora <122997483+sebastianmora29@users.noreply.github.com>
2025-05-24 16:44:32 -0300

    agrega encabezado y menú de navegación
    Incluye el código del <header> y la navegación.

--- index.php
+++ index.php
@@ -11,6 +11,28 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" />
 </head>
 <body>
+  <header>
+  <div class="container">
+      <nav>
+<div class="logo" tabindex="0" aria-label="Logo de la empresa">
+    <img src="images/DEVNODE.jpg" alt="Logo de DevNode" />
+  </div>  
+  <ul class="nav-list">
+    <li><a href="#inicio">Inicio</a></li>
+    <li><a href="#nosotros">Nosotros</a></li>
+    <li><a href="#equipo">Equipo</a></li>
+    <li><a href="#idiomas">Idiomas</a></li>
+    <li><a href="#proyectos">Proyectos</a></li>
+    <li><a href="#herramientas">Herramientas</a></li>
+    <li><a href="#testimonios">Testimonios</a></li>
+    <li><a href="#contacto">Contacto</a></li>
+  </ul>
+  <div class="login-container">
+    <a href="login.php">Login</a>
+  </div>
+</nav>
+  </div>
+</header>
   
 </body>
 </html>
