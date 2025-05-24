c95d445f3f2f3ed59300f3965a79472316d629d2
Sebastian Mora <122997483+sebastianmora29@users.noreply.github.com>
2025-05-24 17:31:41 -0300

    agrega pie de página
    Incluye el código del <footer>.

--- index.php
+++ index.php
@@ -149,6 +149,16 @@
   
 </main>
 
+<footer>
+  <div class="container">
+    <div class="social-links">
+      <a href="https://www.instagram.com/devnode25" target="_blank" title="Instagram"><i class="fab fa-instagram"></i></a>
+      <a href="https://www.linkedin.com/in/dev-node-271405363" target="_blank" title="LinkedIn"><i class="fab fa-linkedin"></i></a>
+      <a href="https://github.com/JuValeria" target="_blank" title="GitHub"><i class="fab fa-github"></i></a>
+    </div>
+    <p>&copy; <?php echo date("Y"); ?> DevNode. Todos los derechos reservados.</p>
+  </div>
+</footer>
   
 </body>
 </html>
