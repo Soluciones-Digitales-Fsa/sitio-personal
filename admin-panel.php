ba0c76b05d3636864ed6fc5b4c8f4a0db4153fa0
Sebastian Mora <122997483+sebastianmora29@users.noreply.github.com>
2025-05-25 14:18:16 -0300

    agrega el link a estilos personalizados y el encabezado
    Incluye en el encabeza el boton para cerrar sesion

--- admin-panel.php
+++ admin-panel.php
@@ -6,8 +6,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Panel de Administrador</title>
+    <link rel="stylesheet" href="styles/estilos-admin.css"/>
 </head>
 <body>
+<header>
+    <h1>Panel de Administrador</h1>
+    <a href="logout.php">Cerrar sesión</a>
+</header>
     
 </body>
 </html>
